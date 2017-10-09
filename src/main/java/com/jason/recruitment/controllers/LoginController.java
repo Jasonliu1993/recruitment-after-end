9c72717d746631467163b878a257bb16869eb526
Jason <ozil.liu2@gmail.com>
2017-10-09 23:46:44 +0800

    main

--- src/main/java/com/jason/recruitment/controllers/LoginController.java
+++ src/main/java/com/jason/recruitment/controllers/LoginController.java
@@ -6,6 +6,8 @@ import com.jason.recruitment.domain.UserAdmin;
 import com.jason.recruitment.exception.StatusCode;
 import com.jason.recruitment.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,16 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by Jason on 17/09/2017.
  */
 
-@RestController
-@RequestMapping(value = "/login")
+@Controller
 public class LoginController {
 
     @Autowired
     LoginService loginService;
 
+    @GetMapping("/index")
+    public String index(ModelMap modelMap) {
+        return "index";
+    }
+
     @Authentication
-    @GetMapping(value = "/authenticate")
-    Message getUserAdmin(String id) {
+    @GetMapping(value = "/login")
+    public Message getUserAdmin(String id) {
         Message<UserAdmin> message = new Message<UserAdmin>();
 
         message.setCode(StatusCode.SUCCESS_CODE.getCode());
